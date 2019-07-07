161304f63a6590df48f37788acb9a935de04c275
HocChr <christian@christianPC>
2019-07-07 18:40:54 +0200

    prepare controller and connections

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->lineEdit, SIGNAL(textChanged(const QString &)), this, SLOT(onEditBinar()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::onEditBinar()
+{
+    qDebug() << "Hallöle";
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,11 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+ private slots:
+    void onEditBinar();
+
+
+
 private:
     Ui::MainWindow *ui;
 };
